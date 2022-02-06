fee8e8f8765fc0e4d0ed454caccb46d5995a383a
ugrsen <93542523+ugrsen@users.noreply.github.com>
2022-02-06 11:09:43 +0530

    Add files via upload
    commit

---
+++ index.js
@@ -0,0 +1,38 @@
+*{
+    margin: 0px;
+}
+body{
+    background-color: black;
+    display: block;
+    text-align: center;
+}
+section{
+    border: 2px solid black;
+    background-color: white;
+    margin: 10px auto;
+    width:210mm;
+    height: 297mm;
+}
+main{
+    border: 2px solid black;
+    border-top: 2px solid  black(240, 22, 193);
+    border-bottom: 2px solid black(240, 22, 193);
+    margin: 90px 90px 0px 90px;
+    height: 279mm -100px;
+}
+h1{
+    font-family: Impact, 'Haettenschweiler', Arial Narrow Bold, sans-serif;
+    font-size: 35px;
+}
+.data{
+    text-align: justify;
+    margin-top: 100px;
+}
+div p:first-child::first-letter{
+    font-size: 5rem;
+    float: left;
+    line-height: 0.8;
+}
+h3{
+    font-family:  'Times New Roman';
+}
